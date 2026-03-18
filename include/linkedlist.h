97e505f1ffc16f296423149fa0f7aba60e9bdcd6
bird00101011 <sc2024228@outlook.com>
2026-03-18 13:03:37 +0800

    之前的linkedlist操作的元素都是固定长度，我这是要处理不同数据长度的数据，不然hashmap就没法写了，写了也是个智障hashmap

--- include/linkedlist.h
+++ include/linkedlist.h
@@ -3,51 +3,52 @@
 #define LINKEDLIST_H
 
 typedef struct Node {
-  void *ele;
-  struct Node *prev;
-  struct Node *next;
+  void* ele;
+  long ele_size;
+  struct Node* prev;
+  struct Node* next;
 } LinkedListNode, *LPLinkedListNode;
 
 typedef struct {
   LPLinkedListNode lp_head;
   LPLinkedListNode lp_tail;
   long eles_num;
-  long ele_size;
-  int (*copy_func)(void *, void *);
-  int (*compare_func)(void *, void *);
-  int (*free_func)(void *);
+  int (*copy_func)(void*, void*);
+  int (*compare_func)(void*, void*);
+  int (*free_func)(void*);
 } LinkedList, *LPLinkedList;
 
-__declspec(dllexport) int LinkedList_init(LPLinkedList lp_ll, long ele_size,
-                                          int (*copy_func)(void *, void *),
-                                          int (*compare_func)(void *, void *),
-                                          int (*free_func)(void *));
+__declspec(dllexport) int LinkedList_init(LPLinkedList lp_ll,
+                                          int (*copy_func)(void*, void*),
+                                          int (*compare_func)(void*, void*),
+                                          int (*free_func)(void*));
 
 __declspec(dllexport) int LinkedList_free(LPLinkedList lp_ll);
 
 __declspec(dllexport) int LinkedList_insert(LPLinkedList lp_ll, long pos,
-                                            void *ele);
+                                            void* ele, long* ele_size);
 
 __declspec(dllexport) int LinkedList_del_by_pos(LPLinkedList lp_ll, long pos);
 
-__declspec(dllexport) int LinkedList_del_by_ele(LPLinkedList lp_ll, void *ele,
+__declspec(dllexport) int LinkedList_del_by_ele(LPLinkedList lp_ll, void* ele,
+                                                long* ele_size,
                                                 LPDynaArray lp_poses);
 
 __declspec(dllexport) int LinkedList_get_by_pos(LPLinkedList lp_ll, long pos,
-                                                void *ele);
+                                                void* ele, long* ele_size);
 
-__declspec(dllexport) void *LinkedList_get_addr_by_pos(LPLinkedList lp_ll,
+__declspec(dllexport) void* LinkedList_get_addr_by_pos(LPLinkedList lp_ll,
                                                        long pos);
 
 __declspec(dllexport) int LinkedList_get_pos_by_ele(LPLinkedList lp_ll,
-                                                    void *ele,
+                                                    void* ele, long ele_size,
                                                     LPDynaArray lp_poses);
 
 __declspec(dllexport) int LinkedList_edit_by_pos(LPLinkedList lp_ll, long pos,
-                                                 void *ele);
+                                                 void* ele, long ele_size);
 
-__declspec(dllexport) int LinkedList_edit_by_ele(LPLinkedList lp_ll,
-                                                 void *old_ele, void *new_ele,
-                                                 LPDynaArray lp_poses);
+__declspec(dllexport) int LinkedList_edit_by_ele(
+    LPLinkedList lp_ll, void* old_ele, long old_ele_size, void* new_ele,
+    long new_ele_size, LPDynaArray lp_poses);
 
 #endif
